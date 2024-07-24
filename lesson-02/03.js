b30def1a5c80e3a7e2055b8d54ff02cd14833d5f
Oleg Parhomenko <shocked194@gmail.com>
2024-07-24 21:29:47 +0300

    first try

--- lesson-02/03.js
+++ lesson-02/03.js
@@ -4,6 +4,18 @@
  * Результат сохраните в переменной grade.
  */
 
-const score = 70 // тестовое значение, можно изменять
+const score = 93 // тестовое значение, можно изменять
 let grade
+if (0 <= score && score <= 49){
+	grade = 'F'
+} else if (50 <= score && score <= 69){
+	grade = 'D'
+} else if (70 <= score && score <= 79){
+	grade = 'C'
+} else if (80 <= score && score <= 89){
+	grade = 'B'
+} else {
+	grade = 'A'
+}
+alert (grade)
 // your code
