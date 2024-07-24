6075f0127fddbd41719ff79510254d5c5a666f13
Oleg Parhomenko <shocked194@gmail.com>
2024-07-24 21:07:27 +0300

    try?

--- lesson-02/02.js
+++ lesson-02/02.js
@@ -9,13 +9,15 @@
 
 let dice1 = Math.floor(Math.random() * 7)// your code
 let dice2 = Math.floor(Math.random() * 7)// your code
-let isWinningDouble = ((dice1 === dice2) > 3)// your code
+let isWinningDouble// your code
 
 
 console.log('Первый бросок: ' + dice1)
 console.log('Второй бросок: ' + dice2)
-if (isWinningDouble) {
+if (dice1 == dice2 && dice1 > 3) {
+  isWinningDouble = true
   console.log('Выигрышный дубль!')
 } else {
+  isWinningDouble = false
   console.log('Не выигрышный дубль.')
 }
