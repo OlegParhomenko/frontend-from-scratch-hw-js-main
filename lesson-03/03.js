ae542d2143f3af1d17aaa1adbd8fc011cc93c1c8
Oleg Parhomenko <shocked194@gmail.com>
2024-07-31 19:56:02 +0300

    lesson3 tq

--- lesson-03/03.js
+++ lesson-03/03.js
@@ -1,3 +1,12 @@
 // Разработайте функцию findLargest, которая принимает три числа и возвращает наибольшее из них.
 
-function findLargest() {}
+function findLargest(numberOne, numberTwo, numberThree) {
+  if (numberOne > numberTwo && numberOne > numberThree) {
+    return numberOne
+  } else if (numberTwo > numberOne && numberTwo > numberThree) {
+    return numberTwo
+  } else {
+    return numberThree
+  }
+}
+
