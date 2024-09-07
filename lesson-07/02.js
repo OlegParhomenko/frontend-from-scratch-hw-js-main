b4cf3bf389e93259cf8800f0b82d48c0066d6ea9
Oleg Parhomenko <shocked194@gmail.com>
2024-09-07 21:11:11 +0300

    3try

--- lesson-07/02.js
+++ lesson-07/02.js
@@ -3,15 +3,15 @@
  * Если строка является числом, функция должна возвращать true, в противном случае - false.
  */
 
-// const mystr = ' '
-// function isNumeric(str){
-//   return +str ? true : false
-// }
-
 
 function isNumeric(str){
-  return Number(str)
+  return +str ? true : false
 }
+
+
+// function isNumeric(str){
+//   return Number(str)
+// }
 
 // console.log(isNumeric("123")) // Ожидаемый результат: true
 // console.log(isNumeric("12.3")) // Ожидаемый результат: true
